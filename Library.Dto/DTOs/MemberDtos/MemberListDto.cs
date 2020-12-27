737152f53e271c19655312d35dbeda4bbcaf508f
furkan7falcao <54326594+furkan7falcao@users.noreply.github.com>
2020-12-27 17:10:30 +0300

    Üye bilgileri güncelleme işlemleri gerçekleştirildi

--- Library.Dto/DTOs/MemberDtos/MemberListDto.cs
+++ Library.Dto/DTOs/MemberDtos/MemberListDto.cs
@@ -6,6 +6,8 @@ namespace Library.DTO.DTOs.MemberDtos
     {
         public int Id { get; set; }
         public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
         public string Picture { get; set; }
 
     }
